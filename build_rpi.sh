5374508178bf857837183b8b8d6b8d719812b055
Simon Cornelius <simonjoecornelius@gmail.com>
2026-01-04 20:45:09 +0100

    fix task dependency in build_rpi

--- build_rpi.sh
+++ build_rpi.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-cross build --bin lebron --target aarch64-unknown-linux-gnu --release
-scp target/aarch64-unknown-linux-gnu/release/lebron simon@steward.local:
+cross build --bin lebron --target aarch64-unknown-linux-gnu --release && scp target/aarch64-unknown-linux-gnu/release/lebron simon@steward.local:
