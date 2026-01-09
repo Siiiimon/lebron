23802391f779092efd18b6dd0890c20422822853
Simon Cornelius <simonjoecornelius@gmail.com>
2026-01-09 21:51:57 +0100

    feat: add audio asset pre-processing

--- process_assets.sh
+++ process_assets.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Cleaning previous assets..."
-rm -f assets/*.tga
+rm -f assets/*.tga assets/*.raw
 
 for input_file in assets/raw/*.bmp; do
     [ -e "$input_file" ] || continue
@@ -14,6 +14,17 @@ for input_file in assets/raw/*.bmp; do
     magick "$input_file" -type TrueColor -depth 5 -compress RLE "$output_file"
 done
 
+for input_file in assets/raw/*.wav; do
+    [ -e "$input_file" ] || continue
+
+    filename=$(basename "$input_file" .wav)
+    output_file="assets/${filename}.raw"
+
+    echo "Processing: $filename..."
+
+    ffmpeg -i "$input_file" -ar 22050 -ac 1 -f s16le -y "$output_file" -v error -stats
+done
+
 echo "Done processing assets!"
 
-du -hc assets/*.tga
+du -hc assets/*.tga assets/*.raw
